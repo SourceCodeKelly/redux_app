8d5b55cd81b8987a302bf4312656bd40c5e5a765
Anthony <96215255+SourceCodeKelly@users.noreply.github.com>
2022-12-10 12:15:44 -0500

    Increment working

--- react-redux/src/store/index.js
+++ react-redux/src/store/index.js
@@ -1,7 +1,14 @@
 import { createStore } from 'redux';
 
 const reducerFn = (state = { counter: 0 },action) => {
+    //Synchronous Function
+    //We should not mutate the original state
 
+    if(action.type === "INC") {
+        return { counter: state.counter + 1 }
+    }
+    return state;
 };
 
 const store = createStore(reducerFn);
+export default store;
